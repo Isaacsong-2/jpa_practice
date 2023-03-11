9b7eb343dde0643270034c37ffc5ba29950ab69c
Isak Song <thddltkr10@naver.com>
2023-03-11 16:56:26 +0900

    컨트롤러 제작

---
+++ src/main/java/com/example/jpa_practice/web/ItemController.java
@@ -0,0 +1,55 @@
+package com.example.jpa_practice.web;
+
+import com.example.jpa_practice.domain.item.Book;
+import com.example.jpa_practice.domain.item.Item;
+import com.example.jpa_practice.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    //상품수정 폼
+    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.GET)
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
+        Item item = itemService.findOne(itemId);
+        model.addAttribute("item", item);
+        return "items/updateItemForm";
+    }
+
+    //상품수정
+    @RequestMapping(value = "/items/{itemId}/edit", method=RequestMethod.POST)
+    public String updateItem(@ModelAttribute("item") Book item){
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
+    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+    @RequestMapping(value = "/itmes/new", method = RequestMethod.GET)
+    public String createForm(){
+        return "items/createItemForm";
+    }
+
+    @RequestMapping(value = "/items/new", method = RequestMethod.POST)
+    public String create(Book item){
+
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+}
